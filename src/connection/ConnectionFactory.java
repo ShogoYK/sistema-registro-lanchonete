e6dee80188d7acad5873049ba31385adf9979df1
ShogoYK <88752020+ShogoYK@users.noreply.github.com>
2022-07-13 13:38:00 -0400

    Update ConnectionFactory.java

--- src/connection/ConnectionFactory.java
+++ src/connection/ConnectionFactory.java
@@ -5,17 +5,17 @@ import java.sql.DriverManager;
 
 public class ConnectionFactory {
 
-	//Nome do usu·rio do mysql
+	//Nome do usu√°rio do mysql
 	private static final String USERNAME = "root";
 	
 	//Senha
 	private static final String PASSWORD = "";
 	
 	//Caminho do bd, porta, nome do bd
-	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/venda";
+	private static final String DATABASE_URL = //Add path"";
 	
 	/*
-	 * Conex„o com o bd
+	 * Conex√£o com o bd
 	 */
 	
 	public static Connection createConnectionToMySQL() throws Exception{
@@ -23,7 +23,7 @@ public class ConnectionFactory {
 		//Faz com q a classe seja carregada pela jvm
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
-		//Cria conex„o com o bd
+		//Cria conex√£o com o bd
 		Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
 		
 		return connection;
@@ -32,12 +32,12 @@ public class ConnectionFactory {
 	
 	public static void main(String[] args) throws Exception{
 		
-		//Recuperar conex„o com o bd
+		//Recuperar conex√£o com o bd
 		Connection con = createConnectionToMySQL();
 		
 		//Teste if null
 		if(con!=null) {
-			System.out.println("Conex„o obtida com sucesso!");
+			System.out.println("Conex√£o obtida com sucesso!");
 			con.close();
 		}
 		
